f272c4a6c2126150a916bd481aff7f9588938786
limenitis <ini.ivi@yandex.ru>
2021-09-25 21:05:20 +0300

    complete lab2 DB

---
+++ Database_design/Lab_Limenitiz/lab2-create-tables/alter_tables.sql
@@ -0,0 +1,6 @@
+ALTER TABLE Clothes
+ADD description varchar(200);
+
+ALTER TABLE Clothes
+DROP COLUMN description;
+
---
+++ Database_design/Lab_Limenitiz/lab2-create-tables/create_tables.sql
@@ -0,0 +1,87 @@
+create table if not exists Clothes (
+    id_clothes int primary key,
+
+    nameClothes nvarchar(20) not null,
+    dataCreate date not null ,
+    sizeClothes int not null
+);
+
+create table if not exists Performance(
+    id_performance int primary key,
+
+    namePerformance nvarchar(20) not null,
+    datePerformance date not null
+);
+
+create table if not exists Role (
+    id_role int primary key,
+
+    id_performance int not null,
+    FOREIGN KEY (id_performance) REFERENCES Performance (id_performance)
+        on delete cascade
+        on update restrict,
+
+    nameRole nvarchar(20)
+);
+
+create table if not exists Author (
+    id_author int primary key,
+
+    firstNameAuthor nvarchar(20),
+    secondNameAuthor nvarchar(20),
+    thirdNameAuthor nvarchar(20)
+);
+
+create table if not exists Type_Detail (
+    id_type_detail int primary key,
+
+    nameType nvarchar(10)
+);
+
+create table if not exists Concrete_Detail (
+    id_concrete_detail int primary key,
+
+    id_type_detail int not null,
+    FOREIGN KEY (id_type_detail) REFERENCES Type_Detail (id_type_detail)
+        on delete cascade
+        on update cascade,
+
+    colorDetail varchar(6) #hex
+);
+
+create table if not exists Clothes_Role (
+    id_clothes int not null,
+    FOREIGN KEY (id_clothes) REFERENCES Clothes (id_clothes)
+        on delete cascade
+        on update cascade,
+
+    id_role int not null,
+    FOREIGN KEY (id_role) REFERENCES Role (id_role)
+        on delete cascade
+        on update cascade
+);
+
+create table if not exists Clothes_Author (
+    id_clothes int not null,
+    FOREIGN KEY (id_clothes) REFERENCES Clothes (id_clothes)
+        on delete cascade
+        on update restrict,
+
+    id_author int not null,
+    FOREIGN KEY (id_author) REFERENCES Author (id_author)
+        on delete restrict
+        on update restrict
+);
+
+create table if not exists Clothes_Concrete_Detail (
+    id_clothes int not null,
+    FOREIGN KEY (id_clothes) REFERENCES Clothes (id_clothes)
+        on delete cascade
+        on update cascade,
+
+    id_concrete_detail int not null,
+    FOREIGN KEY (id_concrete_detail) REFERENCES Concrete_Detail (id_concrete_detail)
+        on delete cascade
+        on update cascade
+);
+
