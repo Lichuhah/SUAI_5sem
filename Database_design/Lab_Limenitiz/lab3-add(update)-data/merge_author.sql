a10b5159ac5f571ac0999dc33bea79893c54f09c
limenitis <ini.ivi@yandex.ru>
2021-09-26 15:31:53 +0300

    Навалил гавна

--- Database_design/Lab_Limenitiz/lab3-add(update)-data/merge_author.sql
+++ Database_design/Lab_Limenitiz/lab3-add(update)-data/merge_author.sql
@@ -1,2 +1,8 @@
-merge into author
-    with 
+#
+#
+# merge author as base_table
+# using clothes as source_table
+#     on (base_table.id_author = source_table.id_clothes)
+# when mathed then
+#     update set
+
---
+++ Fundamentals_of_Software_Engineering/front/lk/common/src/commonMain/kotlin/org/suai/common/model/role/deanatJavaModel.java
@@ -0,0 +1,12 @@
+package org.suai.common.model.role;
+
+import org.jetbrains.annotations.NotNull;
+import org.suai.common.model.*;
+
+public class deanatJavaModel extends userModel{
+
+    public deanatJavaModel() {
+        super();
+    }
+}
+
