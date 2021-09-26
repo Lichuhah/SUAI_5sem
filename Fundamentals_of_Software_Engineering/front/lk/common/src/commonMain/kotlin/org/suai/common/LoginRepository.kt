a333b3e5b0f2f49569717a94ea27289d8e00ed07
TessierAshpoule256 <tessier_ashpoule_2135@list.ru>
2021-09-26 22:18:16 +0300

    job v0.1.1

--- Fundamentals_of_Software_Engineering/front/lk/common/src/commonMain/kotlin/org/suai/common/LoginRepository.kt
+++ Fundamentals_of_Software_Engineering/front/lk/common/src/commonMain/kotlin/org/suai/common/LoginRepository.kt
@@ -1,17 +1,15 @@
 package org.suai.common
 
-import okhttp3.MediaType
+import mu.KotlinLogging
+import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.suai.common.model.loginModel
 import java.io.IOException
 
 
 object LoginRepository {
-    fun getResponse(data: loginModel?): Response {
+    fun getResponse(data: loginModel): Response {
         return try {
             Response(sendLoginData(data))
         } catch (e: Exception) {
@@ -29,38 +27,18 @@ object LoginRepository {
         val body = """{"login":"${data.login}", "password":"${data.password}"}""".toRequestBody(mediaType)
         val request = Request
             .Builder()
-            .url("http://lichuhasite.somee.com/Student/Login")
+            .url("http://lichuhasite.somee.com/Login/Auth")
             .method("POST", body)
             .addHeader("Content-Type", "application/json")
             .build()
-        val response = client.newCall(request).execute()
 
-        return response.body?.string() ?: "fuck"
-
-//        val uri = "http://www.lichuhasite.somee.com/Student/Login"
-//        val client = HttpClient.newBuilder().build()
-//        val gson = Gson()
-//        println(gson.toJson(data))
-//        val request = HttpRequest.newBuilder()
-//            .uri(URI.create(uri))
-//            .setHeader("Content-Type", "application/json")
-//            .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(data)))
-//            .build()
-//        val response: HttpResponse<*> = client.send(request, HttpResponse.BodyHandlers.discarding())
-//        KotlinLogging.logger {}.info { "response.headers()= ${response.headers()}" }
-//        KotlinLogging.logger {}.info { "response.body()= ${response.body()}" }
-//        KotlinLogging.logger {}.info { "response.previousResponse()= ${response.previousResponse()}" }
-//        KotlinLogging.logger {}.info { "response.uri()= ${response.uri()}" }
-//        KotlinLogging.logger {}.info { "response.statusCode()= ${response.statusCode()}" }
-//        return response.toString()
-
-//        String       postUrl       = "http://www.lichuhasite.somee.com/Login/Add";
-//        Gson gson          = new Gson();
-//        HttpClient httpClient    = HttpClientBuilder.create().build();
-//        HttpPost     post          = new HttpPost(postUrl);
-//        StringEntity postingString = new StringEntity(gson.toJson(pojo1));//gson.tojson() converts your pojo to json
-//        post.setEntity(postingString);
-//        post.setHeader("Content-type", "application/json");
-//        HttpResponse response = httpClient.execute(post);
+        val responseBody = client.newCall(request).execute().body
+        return if ( responseBody != null ) {
+            val response = responseBody.string()
+            KotlinLogging.logger {}.info { "response=$response" }
+            response
+        } else {
+            "Error response"
+        }
     }
 }
--- Fundamentals_of_Software_Engineering/front/lk/common/src/commonMain/kotlin/org/suai/common/screen/LoginScreen.kt
+++ Fundamentals_of_Software_Engineering/front/lk/common/src/commonMain/kotlin/org/suai/common/screen/LoginScreen.kt
@@ -53,43 +53,45 @@ fun LoginScreen() {
         item {
             Text(
                 text     = "Личный кабинет ГУАП",
-                fontSize = 26.sp
+                fontSize = 26.sp,
+                maxLines = 1
             )
         }
         item {
             OutlinedTextField(
                 value         = login,
                 onValueChange = { login = it },
-                label         = { Text("Логин") }
+                label         = { Text("Логин") },
+                maxLines = 1
             )
             OutlinedTextField(
                 value                = password,
                 onValueChange        = { password = it },
                 label                = { Text("Пароль") },
                 visualTransformation = PasswordVisualTransformation(),
+                maxLines = 1
                 )
         }
         item {
             Box (
                 Modifier
-                    .fillMaxWidth(0.25f)
+                    .fillMaxWidth(0.35f)
                     .padding(top = 3.dp)
                     .clickable {
                     registerOn = !registerOn
                     if ( registerOn ) {
                         buttonText = "Регистрация"
-                        registerButtonText = "^"
+                        registerButtonText = "      ^      "
                     }
                     else {
                         buttonText = "Вход"
-                        registerButtonText = "v"
+                        registerButtonText = "      v      "
                     }
                 }
             ) {
                     Text(
                         registerButtonText,
-                        fontSize = 16.sp,
-                        textAlign = TextAlign.End
+                        fontSize = 16.sp
                     )
             }
         }
@@ -97,31 +99,35 @@ fun LoginScreen() {
             item {
                 Box( modifier = Modifier.fillMaxWidth(0.25f)) {
                     Text(
-                        text     = "Регистрация",
-                        fontSize = 16.sp,
+                        text      = "Регистрация",
+                        fontSize  = 16.sp,
                         textAlign = TextAlign.Start
                     )
                 }
                 OutlinedTextField(
                     value         = first_name,
                     onValueChange = { first_name = it },
-                    label         = { Text("Имя") }
+                    label         = { Text("Имя") },
+                    maxLines = 1
                 )
                 OutlinedTextField(
                     value         = second_name,
                     onValueChange = { second_name = it },
-                    label         = { Text("Фамилия") }
+                    label         = { Text("Фамилия") },
+                    maxLines = 1
                 )
                 OutlinedTextField(
                     value         = other_name,
                     onValueChange = { other_name = it },
-                    label         = { Text("Отчество") }
+                    label         = { Text("Отчество") },
+                    maxLines = 1
                 )
                 OutlinedTextField(
                     value         = second_password,
                     onValueChange = { second_password = it },
                     label         = { Text("Повторите ввод пароля") },
                     visualTransformation = PasswordVisualTransformation(),
+                    maxLines = 1
                 )
             }
         item {
