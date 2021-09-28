ba89bee87a6c93aed9bae7ab5c7e8ed22cf06c82
limenitis <ini.ivi@yandex.ru>
2021-09-28 14:11:21 +0300

    Фиксы для 2 лабы по бд

--- Database_design/Lab_Limenitiz/lab2-create-tables/create_tables.sql
+++ Database_design/Lab_Limenitiz/lab2-create-tables/create_tables.sql
@@ -1,52 +1,51 @@
 create table if not exists Clothes (
-    id_clothes int primary key auto_increment,
+    id_clothes int not null auto_increment primary key,
 
-    nameClothes nvarchar(20) not null,
+    nameClothes varchar(20) not null,
     dataCreate date not null ,
     sizeClothes int not null
 );
 
 create table if not exists Performance(
-    id_performance int primary key auto_increment,
+    id_performance int not null auto_increment primary key,
 
-    namePerformance nvarchar(20) not null,
+    namePerformance varchar(20) not null,
     datePerformance date not null
 );
 
 create table if not exists Role (
-    id_role int primary key auto_increment,
+    id_role int not null auto_increment primary key,
 
     id_performance int not null,
     FOREIGN KEY (id_performance) REFERENCES Performance (id_performance)
         on delete cascade
         on update restrict,
 
-    nameRole nvarchar(20)
+    nameRole varchar(20)
 );
 
 create table if not exists Author (
-    id_author int not null auto_increment primary key, #TODO: переписать все ключи как здесь
+    id_author int not null auto_increment primary key,
 
-    firstNameAuthor nvarchar(20) default null,
-    secondNameAuthor nvarchar(20) default null,
-    thirdNameAuthor nvarchar(20) default null
+    firstNameAuthor varchar(20) default null,
+    secondNameAuthor varchar(20) default null,
+    thirdNameAuthor varchar(20) default null
 )
 ENGINE = InnoDB
-DEFAULT CHARSET = utf8bm4
+#DEFAULT CHARSET = utf8bm4
 AUTO_INCREMENT = 1;
 
-
 create table if not exists Type_Detail (
-    id_type_detail int primary key auto_increment,
+    id_type_detail int not null auto_increment primary key,
 
-    nameType nvarchar(10)
+    nameType varchar(10)
 );
 
 create table if not exists Concrete_Detail (
-    id_concrete_detail int primary key auto_increment,
+    id_concrete_detail int not null auto_increment primary key,
 
     id_type_detail int not null,
-    constraint FOREIGN KEY (id_type_detail) #TODO: добавить везде такой constraint
+    constraint FOREIGN KEY (id_type_detail)
     REFERENCES Type_Detail (id_type_detail)
         on delete cascade
         on update cascade,
@@ -55,12 +54,12 @@ create table if not exists Concrete_Detail (
 );
 
 create table if not exists Clothes_Role (
-    id_clothes int not null,
+    id_clothes int not null ,
     FOREIGN KEY (id_clothes) REFERENCES Clothes (id_clothes)
         on delete cascade
         on update cascade,
 
-    id_role int not null,
+    id_role int not null ,
     FOREIGN KEY (id_role) REFERENCES Role (id_role)
         on delete cascade
         on update cascade
@@ -90,3 +89,10 @@ create table if not exists Clothes_Concrete_Detail (
         on update cascade
 );
 
+# SELECT default_character_set_name
+# FROM information_schema.SCHEMATA
+# WHERE schema_name = "database_design_course";
+
+# select *
+# from information_schema.table_constraints
+# where table_name = "table_Name";
