2137ff98582ea1e38a1a6364947bb32a3c27df6a
limenitis <ini.ivi@yandex.ru>
2021-10-12 01:05:30 +0300

    Первая верся третьей лабы по БД и набросок отчета ОПИ

--- Database_design/Lab_Limenitiz/lab3-add-data/merge_author.sql
+++ Database_design/Lab_Limenitiz/lab3-add-data/merge_author.sql
@@ -1,8 +1,26 @@
+# Допустим мы создали новую таблицу author_new
+# и в нее добавили отношение date_born и заполнили таблицу
 #
-#
-# merge author as base_table
-# using clothes as source_table
-#     on (base_table.id_author = source_table.id_clothes)
-# when mathed then
-#     update set
+# затем мы изменяем старую таблицу и добавляем туда новые изменения
 
+create table if not exists author_new (
+    id_author int not null auto_increment primary key,
+
+    firstNameAuthor varchar(20) default null,
+    secondNameAuthor varchar(20) default null,
+    thirdNameAuthor varchar(20) default null,
+
+    # new column
+    date_born date default null;
+)
+AUTO_INCREMENT = 1;
+
+alter table clothes
+add column date_born date default null;
+
+merge into clothes as c
+    using clothes_new as cn
+    on c.id_clothes = cn.id_clothes
+
+    when NOT MATCHED then update
+    set c.date_born = cn.date_born;
