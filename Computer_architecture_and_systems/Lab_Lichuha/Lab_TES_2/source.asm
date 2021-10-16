9f53e6cbe12165aecd3c3d572731295fc6ce7099
Lichuhah <belovanton.work@gmail.com>
2021-10-16 19:33:11 +0300

    Отчеты1

--- Computer_architecture_and_systems/Lab_Lichuha/Lab_TES_2/source.asm
+++ Computer_architecture_and_systems/Lab_Lichuha/Lab_TES_2/source.asm
@@ -2,9 +2,9 @@
 _c_int00:
 	.data
 	
-array1:		.short 1,1,1
-array2: 	.short 1,2,3
-arrayRes:	.short 0,0,0
+array1:		.int 1,1,1
+array2: 	.int 1,2,3
+arrayRes:	.int 0,0,0
 size	.set 3	
 
 	.text
@@ -12,26 +12,26 @@ size	.set 3
 		MVKL .S1 array1,A3 		;загружаем первый вектор в A3   	
  		MVKH .S1 array1,A3 		;
  		
- 		MVKL .S1 array2,A4		;загружаем второй вектор в A4
- 		MVKH .S1 array2,A4		;
+ 		MVKL .S1 array2,A7		;загружаем второй вектор в A4
+ 		MVKH .S1 array2,A7		;
 
-		MVKL .S1 arrayRes,A5	;загружаем резлультат в A5
- 		MVKH .S1 arrayRes,A5	;
+		MVKL .S1 arrayRes,A1	;загружаем резлультат в A1
+ 		MVKH .S1 arrayRes,A1	;
 
  		MVK .S1 size,A2 		;загружаем размер массива в А2		
  	 
-		MVK .S1 0,A6			;тек эл из первого вектора 
-		MVK .S1 0,A7			;тек эл из второго вектора
-		MVK .S1 0,A8			;тек сумма
+		MVK .S2 0,B2			;тек эл из первого вектора 
+		MVK .S1 0,A5			;тек эл из второго вектора
+		MVK .S1 0,A0			;тек сумма
 		
 LOOP:
 		SUB .L1 A2,1,A2			;уменьшаем счетчик
-		LDW .D1 *A3[A2],A6		;загружаем тек эл первого вектора
+		LDW .D1 *A3[A2],B2		;загружаем тек эл первого вектора
 		NOP 4
-		LDW .D1 *A4[A2],A7		;загружаем тек эл второго вектора
+		LDW .D1 *A7[A2],A5		;загружаем тек эл второго вектора
 		NOP 4
-		ADD .L1 A6,A7,A8		;получаем сумму тек элементов
-		STW .D1 A8,*A5[A2]		;загружаем сумму в результат
+		ADD .L1X A5,B2,A0		;получаем сумму тек элементов
+		STW .D1 A0,*A1[A2]		;загружаем сумму в результат
 		[A2] B .S1 LOOP
 		NOP 5
 	.end
---
+++ Server_Systems_Development_Technology/Lab_Lichuha/Lab5/src/main/java/com/example/lab2_3/WebController.java
@@ -0,0 +1,46 @@
+package com.example.lab2_3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String MainPage() {
+        return "home";
+    }
+
+    @GetMapping("doctors")
+    public String DocIndex() {
+        return "DoctorIndex";
+    }
+
+    @GetMapping("doctors/view{id}")
+    public String DocGet(@PathVariable int id, Model model){
+        model.addAttribute("docId", id);
+        return "DoctorForm";
+    }
+
+    @GetMapping("patients")
+    public String PatIndex() {
+        return "PatientIndex";
+    }
+
+    @GetMapping("patients/view{id}")
+    public String PatGet(@PathVariable int id, Model model){
+        model.addAttribute("patId", id);
+        return "PatientForm";
+    }
+
+    @GetMapping("appointments")
+    public String AppIndex() {
+        return "AppointmentsIndex";
+    }
+
+    @GetMapping("appointments/view")
+    public String AppGet(){
+        return "AppointmentForm";
+    }
+}
