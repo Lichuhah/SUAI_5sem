a3f44bc6549d13e4836c78c92304853689dd07a2
Lichuhah <belovanton.work@gmail.com>
2021-11-10 11:28:41 +0300

    сука не тот курсач

--- Applied_Optimization_Models/Lab_Lichuha/KursWork/KursWork/KursWork/KursWork1/PlanItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KursWork
-{
-    public class PlanItem
-    { 
-        public Section Section { get; set; }
-        public Cargo Cargo { get; set; }
-        public double Count { get; set; }
-    }
-}
